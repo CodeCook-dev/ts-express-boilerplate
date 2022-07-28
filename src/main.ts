635e09d3cca2e423663da09498b75b345cb6f4ff
zKnight223 <lazyknight223@gmail.com>
2022-07-28 09:55:04 -0400

    amend issue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { Server } from './server';
+import { Server } from "./server";
 
 class Main {
-	public start():void {
-		// Initialize server with port 3000 from params
-		const server = new Server(3000);
-		server.start();
-	}
+  public start(): void {
+    // Initialize server with port 3000 from params
+    const server = new Server(Number(process.env.SERVER_PORT) || 3000);
+    server.start();
+  }
 }
 
 const main = new Main();
